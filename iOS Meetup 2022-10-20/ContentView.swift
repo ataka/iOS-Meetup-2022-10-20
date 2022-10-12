09e282afb15c0a7e93c01fc7f860f3953ca40959
cw-ataka <ataka@chatwork.co.jp>
2022-10-12 17:22:33 +0900

    Add minimum list

--- iOS Meetup 2022-10-20/ContentView.swift
+++ iOS Meetup 2022-10-20/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("SwiftUI")
+                Text("Storyboard (not localized)")
+                Text("Storyboard (localized)")
+            }
+            .navigationTitle("iOS Meetup")
+        }
     }
 }
 
