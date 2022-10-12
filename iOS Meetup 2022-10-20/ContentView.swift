c0b310f089280c549a94efd55cc4558074be4aeb
cw-ataka <ataka@chatwork.co.jp>
2022-10-12 16:41:11 +0900

    Initial Commit

---
+++ iOS Meetup 2022-10-20/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Meetup 2022-10-20
+//
+//  Created by 安宅正之 on 2022/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Meetup 2022-10-20/iOS_Meetup_2022_10_20App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Meetup_2022_10_20App.swift
+//  iOS Meetup 2022-10-20
+//
+//  Created by 安宅正之 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Meetup_2022_10_20App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
